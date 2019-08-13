76af9a81633627a21cdbb4d7463072462ff2bcf3
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-08-13 16:50:39 +0800

    part 6b_filter exercise

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,27 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {RadioGroup, Radio} from 'react-radio-group'
+import {vote, toggleImportanceOf} from '../reducers/anecdoteReducer' 
 
 const AnecdoteList = (props) => {
 
+    const [ filter, setFilter ] = useState('all')
     const anecdotes = props.store.getState()
     
-    const vote = (id) => {
-        props.store.dispatch({
-          type: 'ADD_VOTE',
-          data: { id }
-        })
+    // const voteForAnecdote = (id) => {
+    //     props.store.dispatch ({
+    //         type: 'ADD_VOTE',
+    //         data: { id }
+    //     })
+    // }
+
+    // const toggleImportanceOf = (id) => {
+    //     props.store.dispatch({
+    //         type: 'TOGGLE_IMPORTANCE',
+    //         data: { id }
+    //       })  
+    // }
+
+    const selectAnecdotes = (filterValue) => {
+        switch(filterValue) {
+            case 'imp'      :   return anecdotes.filter(anecdote => anecdote.important).sort(compare)
+            case 'notimp'   :   return anecdotes.filter(anecdote => !anecdote.important).sort(compare) 
+            default         :   return anecdotes.sort(compare)
+        }
     }
 
+    const setFilterValue = (selectedValue) => {
+        setFilter(selectedValue)
+    }
+
+    const compare = (anecdoteOne, anecdoteTwo) => anecdoteTwo.votes - anecdoteOne.votes
+ 
     return (
         <div>
+            <div className="radio">
+                <RadioGroup name="select" onChange={setFilterValue}>
+                    Show All:<Radio value="all" defaultChecked={true} />&nbsp;
+                    Important:<Radio value="imp" />&nbsp;
+                    Not Important:<Radio value="notimp" />&nbsp;
+                </RadioGroup>
+            </div>
             <h2>Anecdotes</h2>
-            {anecdotes.map(anecdote =>
+            {selectAnecdotes(filter).map(anecdote =>
             <div key={anecdote.id}>
                 <div>
-                {anecdote.content}
+                    {anecdote.content}
                 </div>
                 <div>
                 has {anecdote.votes} votes
-                <button onClick={() => vote(anecdote.id)}>vote</button>
+                <button onClick={() => props.store.dispatch(vote(anecdote.id))}>vote</button>
+                </div>
+                <div>
+                    Important: <input 
+                            type='checkbox' 
+                            name='filter' 
+                            onChange={() => props.store.dispatch(toggleImportanceOf(anecdote.id))}
+                            defaultChecked={anecdote.important}
+                            />
                 </div>
             </div>
             )}
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -12,6 +12,7 @@ const getId = () => (100000 * Math.random()).toFixed(0)
 const asObject = (anecdote) => {
   return {
     content: anecdote,
+    important: Math.random() > 0.5,
     id: getId(),
     votes: 0
   }
@@ -23,24 +24,46 @@ const compare = (a, b) => {
   return b.votes - a.votes
 }
 
+export const vote = (id) => {
+  console.log("In vote of anecdoteReducer.js")
+  return ({
+    type: 'ADD_VOTE',
+    data: { id }
+  })
+}
+
 export const createAnecdote = (anecdote) => {
   return ({
     type: 'ADD_ANECDOTE',
     data: asObject(anecdote)
   })
+}
 
+export const toggleImportanceOf = (id) => {
+  return ({
+    type: 'TOGGLE_IMPORTANCE',
+    data: {id}
+  })
 }
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case 'ADD_VOTE'     : {
-                            const anecdotes = [...state]
-                            anecdotes.find(anecdote => anecdote.id === action.data.id).votes++
-                            return anecdotes.sort(compare)
-                          }
+    case 'ADD_VOTE'           : {
+                                  const anecdotes = [...state]
+                                  anecdotes.find(anecdote => anecdote.id === action.data.id).votes++
+                                  return anecdotes.sort(compare)
+                                }
 
-    case 'ADD_ANECDOTE' : return state.concat(action.data).sort(compare)
-    default             : return state.sort(compare)
+    case 'ADD_ANECDOTE'       : return state.concat(action.data).sort(compare)
+
+    case 'TOGGLE_IMPORTANCE'  : {
+                                  const anecdotes = [...state]
+                                  const anecdote = anecdotes.find(anecdote => anecdote.id === action.data.id)
+                                  anecdote.important = !anecdote.important
+                                  return anecdotes.sort(compare)
+                                }
+
+    default                   : return state.sort(compare)
   }
 }
 
